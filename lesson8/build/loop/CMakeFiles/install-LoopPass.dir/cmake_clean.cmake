363da80ec17b368541b3684699a80153727087e6
ehg54 <ehg54@cornell.edu>
2025-03-20 10:22:43 -0400

    get rid of build

--- lesson8/build/loop/CMakeFiles/install-LoopPass.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-LoopPass"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-LoopPass.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
